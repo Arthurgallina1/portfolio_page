a44a095c807d81f33f4108d7f1a298cd59a9b836
Arthur <arthur.gallina@gmail.com>
2019-09-28 17:03:51 -0300

    add stack icons

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,1 @@
-// DOM Selectors
 
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
-    // $('.nav-link').toggleClass('scrolled', $(this).scrollTop() > 150);
-    
-});
